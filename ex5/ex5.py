9c5db3474893daa3a69179c5756e42eb53323e85
BachNguyen <bach.nguyen@banvien.com.vn>
2025-12-01 16:35:57 +0700

    rename hw ex5

---
+++ ex5/ex5.py
@@ -0,0 +1,19 @@
+import sklearn
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# from ydata_profiling import ProfileReport
+
+data = pd.read_csv("diabetes.csv")  # Hiện tại
+
+# profile = ProfileReport(data, title="Diabetes Data Profiling Report")
+# profile.to_file("diabetes_data_profile.html")
+target = "Outcome"
+x = data.drop(target, axis=1)
+# print(x.head())
+y = data[target]
+# print(y.head())
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state= 1402)
+print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
